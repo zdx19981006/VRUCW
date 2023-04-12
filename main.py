4eca1897c5fa2dcca9c7908071e25c4cb6c1d96f
rhh <745491476@qq.com>
2023-04-12 12:07:56 +0800

    second

--- main.py
+++ main.py
@@ -58,14 +58,26 @@ class Agent:
             vehicle_Loc = self.get_location(vehicle)  # 目标车辆的位置
             vehicle_x_speed = self.get_velocity(vehicle)[1]  # 目标车辆横向速度
             vehicle_y_speed = self.get_velocity(vehicle)[2]  # 目标车辆纵向速度
+            vehicle_x_acc=self.get_acceleration[1] #目标车辆横向加速度
+            vehicle_y_acc=self.get_acceleration[2] #目标车辆纵向加速度
             # vechile_bmax：目标车辆最大制动加速度  egoLoc：本车位置  ego_veloc：本车当前速度  t1平均反应时间
             # ego_usual_amax 当前车辆正常减速度
+            #纵向安全距离
             d1 = ego_y_veloc * self.t1 + 0.5 * ego_y_acc * self.t1 * self.t1 + (ego_y_veloc + ego_y_acc * self.t1) * (
                         ego_y_veloc + ego_y_acc * self.t1) / (2 * self.ego_usual_amax)
             d2 = vehicle_y_speed * vehicle_y_speed / (2 * self.vechile_bmax)
             y_dmin = d1 - d2  # 纵向距离
-
             if y_dmin <= 0:
+                return True
+
+
+            #横向安全距离
+            ego_vt_max=ego_x_veloc+ego_X_acc*self.t1 #反应时间内，本车的最大横向速度
+            veh_vt_max=vehicle_x_speed+vehicle_x_acc*self.t1 #反应时间内 目标车辆的最大横向速度
+            ego_vt_dis=((ego_x_veloc+ego_vt_max)/2)*self.t1+ego_vt_max*ego_vt_max/(2*self.ego_usual_amax) #速度减为0时本车行驶过的路程
+            veh_vt_dis=((vehicle_x_speed+veh_vt_max)/2)*self.t1+veh_vt_max*veh_vt_max/(2*self.ego_usual_amax) #速度减为0时目标车辆行驶过的距离
+            dis_lat=ego_vt_dis+veh_vt_dis+ego_x_veloc #横向安全距离
+            if(dis_lat<=0):
                 return True
 
         return True
